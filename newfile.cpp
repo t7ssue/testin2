61daefba944b4f67cc23e0a7b8ab2c6ce99772b0
t7ssue <minthetkyaw.edu@gmail.com>
2024-08-27 14:24:04 +0630

    changes tesing

---
+++ newfile.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    cout<<"Tesing pull project"<<endl;
+    cout<<"Tesing pull project"<<endl;
+    cout<<"Tesing pull project"<<endl;
+    cout<<"Tesing pull project"<<endl;
+    cout<<"Tesing pull project"<<endl;
+
+}
